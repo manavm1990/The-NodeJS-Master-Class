de795456ea9dd00f8468c305e1442bd20ac55ae3
manavm1990 <manavm@visionwebsoft.com>
2018-06-21 14:32:36 -0500

    Rename variable for clarity.

--- rest/lib/handlers.js
+++ rest/lib/handlers.js
@@ -3,7 +3,20 @@
 // Handlers
 const handlers = {};
 
+  const validatedFname =
+  const validatedLname =
+  const validatedFone =
+  const validatedPword =
+  const validatedTos = typeof tos === "boolean" && tos === true ? tos : false;
+    !validatedFname ||
+    !validatedLname ||
+    !validatedFone ||
+    !validatedPword ||
+    !validatedTos
   dataMgr.readDataFile("users", fone, (err, data) => {
+        fname: validatedFname,
+        lname: validatedLname,
+        fone: validatedFone,
 handlers.ping = function ping(d, cb) {
   /* This passes no data. So, as seen below, it will default to an empty object. */
   cb(200);
