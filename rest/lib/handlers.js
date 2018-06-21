d5b0d1d8c9cd26e7cf107417fbb53aa16c075c02
manavm1990 <manavm@visionwebsoft.com>
2018-06-21 15:12:12 -0500

    Create users handler fxn.

--- rest/lib/handlers.js
+++ rest/lib/handlers.js
@@ -1,25 +1,105 @@
 // Dependencies
+const helpers = require("./helpers");
 const crud = require("./crud");
 
 // Handlers
 const handlers = {};
 
+// Users handler
+handlers.users = function users(d, cb) {
+  // Limit the types of requests users can make
+  const accMethodTypes = ["get", "post", "put", "delete"];
+
+  const currMethodType = d.method;
+
+  if (accMethodTypes.indexOf(currMethodType) === -1) {
+    // -1 means that it was not found (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf)
+    cb(405);
+    return;
+  }
+
+  handlers.users[currMethodType](d, cb);
+};
+
+handlers.users = {};
+
+handlers.users.getHandler = function getHandler(d, cb) {
+  // Grab trimmed fields
+  const fname = d.payload.fname.trim();
+  const lname = d.payload.lname.trim();
+  const fone = d.payload.fone.trim();
+  const pword = d.payload.pword.trim();
+  const tos = d.payload.tos.trim();
+
+  // Server-side validation
   const validatedFname =
+    typeof fname === "string" && fname.length > 0 ? fname : false;
   const validatedLname =
+    typeof lname === "string" && lname.length > 0 ? lname : false;
   const validatedFone =
+    typeof fone === "string" && fone.length === 10 ? fone : false;
   const validatedPword =
+    typeof password === "string" && pword.length > 0 ? pword : false;
   const validatedTos = typeof tos === "boolean" && tos === true ? tos : false;
+
+  if (
     !validatedFname ||
     !validatedLname ||
     !validatedFone ||
     !validatedPword ||
     !validatedTos
-  crud.readDataFile("users", fone, (err, data) => {
+  ) {
+    cb(400, { Error: "Missing required fields!" }); // Should send back a data object - not just string.
+    return;
+  }
+
+  /* Make sure user doesn't already have a phone number data file. 
+  We do this by trying to read their data file. */
+  crud.readDataFile("users", validatedFone, (err, data) => {
+    // Fone is name of JSON file
+    /* If error reading, it means that the user doesn't already exist! Good! */
+    if (err) {
       // Hash the password using 'crypto.'
       const hashedPword = helpers.hash(validatedPword);
+
+      // Make sure password got hashed
+      if (!hashedPword) {
+        cb(500, { Error: "Password didn't get hashed!" });
+        return;
+      }
+
+      // Create user object
+      const userObj = {
         fname: validatedFname,
         lname: validatedLname,
         fone: validatedFone,
+        pword: hashedPword
+      };
+
+      // Store the newly created user in their own JSON file witin 'users' directory.
+      crud.createRiteCloseFile(
+        "users",
+        validatedFone,
+        userObj,
+        createUserErr => {
+          if (createUserErr) {
+            console.log(`Error creating the new user: ${err}`);
+            cb(500, { Error: "Unable to create the new user!" });
+            return;
+          }
+
+          cb(200);
+        }
+      );
+    } else {
+      cb(400, { Error: "User already exists!" });
+    }
+  });
+};
+handlers.users.post = function get(d, cb) {};
+handlers.users.put = function get(d, cb) {};
+handlers.users.delete = function get(d, cb) {};
+
 handlers.ping = function ping(d, cb) {
   /* This passes no data. So, as seen below, it will default to an empty object. */
   cb(200);
